7a73305c73985e088fde538248a398c7454e1445
milan.997@hotmail.com <lynyrdskynyrd123>
2018-03-19 21:26:13 +0100

    Proba test

--- app/src/main/java/com/example/alowishusad/androidprojekat/LoginActivity.java
+++ app/src/main/java/com/example/alowishusad/androidprojekat/LoginActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
+    //Test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
