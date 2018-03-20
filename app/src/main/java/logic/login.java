b2e3002a4e38847336b26743a96612db892f5ba7
milan997 <milan.997@hotmail.com>
2018-03-20 12:48:42 +0100

    Vezbe 3, dodat login, nije zavrsen

---
+++ app/src/main/java/logic/login.java
@@ -0,0 +1,14 @@
+package logic;
+
+/**
+ * Created by student on 20.3.18..
+ */
+
+public class login {
+
+    // Logika za proveru logina, dovrsiti
+    public static boolean checkLogin(String userName, String password){
+        return true;
+    }
+
+}
