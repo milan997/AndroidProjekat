b844e78bfd397b09d4dc5f34b459cbd9aa0d48d0
Milan Miljus <milan.997@hotmail.com>
2018-03-25 03:46:47 +0200

    Zavrsen deo 3. domaceg
    Zavrsen CreatePostActivity, jos samo malo da se mitjne ReadPostActivity, zajebani su tabovi, i to je to.

--- app/src/main/java/com/example/alowishusad/androidprojekat/CreatePostActivity.java
+++ app/src/main/java/com/example/alowishusad/androidprojekat/CreatePostActivity.java
@@ -1,7 +1,10 @@
 package com.example.alowishusad.androidprojekat;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreatePostActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class CreatePostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
+    }
+
+    // Function to be called when clicked on "photo" button
+    public void btnOpenCamera(View view) {
+        Intent openCameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Checking if there's at least one camera app
+        if (openCameraIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(openCameraIntent, 1);
+        }
+    }
+
+    public void btnOpenGallery(View view) {
+        Intent openGalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        if (openGalleryIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(openGalleryIntent, 1);
+        }
     }
 }
--- app/src/main/java/com/example/alowishusad/androidprojekat/PostsActivity.java
+++ app/src/main/java/com/example/alowishusad/androidprojekat/PostsActivity.java
@@ -1,7 +1,6 @@
 package com.example.alowishusad.androidprojekat;
 
 import android.content.Intent;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,18 +13,18 @@ public class PostsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_posts);
     }
 
-    public void btnStartsCreatePostActivity (View view) {
-        Intent intent = new Intent(PostsActivity.this, CreatePostActivity.class);
+    public void btnCreatePost(View view) {
+        Intent intent = new Intent(this, CreatePostActivity.class);
         startActivity(intent);
     }
 
-    public void btnStartsReadPostActivity (View view) {
-        Intent intent = new Intent(PostsActivity.this, ReadPostActivity.class);
+    public void btnReadPost(View view) {
+        Intent intent = new Intent(this, ReadPostActivity.class);
         startActivity(intent);
     }
 
-    public void btnStartsSettingsActivity (View view) {
-        Intent intent = new Intent(PostsActivity.this, SettingsActivity.class);
+    public void btnSettings(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
 }
